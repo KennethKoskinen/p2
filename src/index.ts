90be9435e7cff3e646e79167fe43e8e44f24ef17
Kenneth <kennethkoskinen41@gmail.com>
2022-11-27 18:59:09 +0200

    Kasutajate ressursi p2rimine id alusel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express';
+import { servicesVersion } from 'typescript';
 
 const app = express();
 const PORT = 3000;
@@ -26,13 +27,13 @@ const users: IUser[] = [
         password: 'juhan' 
     }
 ];
-
+// For testing API endpoint
 app.get('/api/v1/health', (req: Request, res: Response) => {
     res.status(200).json({
         message: "Tere maailm!",
     });
 });
-
+//List of all accounts endpoint
 app.get('/api/v1/users', (req: Request, res: Response) => {
     res.status(201).json({
         success: true,
@@ -41,8 +42,76 @@ app.get('/api/v1/users', (req: Request, res: Response) => {
     });
 });
 
+//request account info by id
+app.get('/api/v1/users/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id) ;
+    const user = users.find(element => {
+        return element.id === id;
+    })
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: `User not found. `,
+            
+        });
+    }
+    
+    return res.status(200).json({
+        success: true,
+        message: `User. `,
+        data: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+        }
+        
+    });
+});
+//Change account settings
+app.patch('/api/v1/users/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id) ;
+    const {firstName, lastName, email, password} = req.body;
+    const user = users.find(element => {
+        return element.id === id;
+    })
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: `User not found. `,
+            
+        });
+    }
+    if (!firstName && !lastName && !email &&!password) {
+        return res.status(400).json({
+            success: false,
+            message: `Nothing to change. `,
+            
+        });
+    }
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (email) user.email = email;
+    if (password) user.password = password;
+    
+    return res.status(200).json({
+        success: true,
+        message: `User updated. `,
+
+        
+    });
+});
+
+//Creating an account
 app.post('/api/v1/users', (req: Request, res: Response) => {
     const {firstName, lastName, email, password} = req.body;
+    if (!firstName || !lastName || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: `Some data is missing(first name, last name, email, password). `,
+            
+        });
+    }
     const id = users.length + 1; 
     const newUser: IUser = {
         id,
@@ -59,7 +128,7 @@ app.post('/api/v1/users', (req: Request, res: Response) => {
         
     });
 });
-
+//Deleting an account 
 app.delete('/api/v1/users/:id', (req: Request, res: Response) => {
     const id = parseInt(req.params.id) ;
     const index = users.findIndex(element => element.id === id);
